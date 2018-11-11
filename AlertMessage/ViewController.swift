c0f52e3bd06800faa428a128f4017272e1e2b77c
Baranseli <baranseli@baranseli.com>
2018-11-11 15:25:26 +0300

    Initial Commit

---
+++ AlertMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertMessage
+//
+//  Created by Kafkas Baranseli on 11/11/2018.
+//  Copyright © 2018 Baranseli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
